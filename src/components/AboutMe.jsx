bd6d185199826f3fe8c7f020d8ef72b505e0ebb1
Adi2329002 <adityaalok.0088@gmail.com>
2025-11-04 12:42:12 +0530

    Textual Update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,10 +2,10 @@ const AboutMe = () => {
     return (
         <section id="about" className="about-me-section">
             <div className="about-me-left">
-                <h1 className="service-heading">Intrested In</h1>
+                <h1 className="service-heading">Interested In</h1>
                 <div className="service-item">
                     <span className="service-icon">💻</span>
-                    <h3 className="service-title">Cyber Securities</h3>
+                    <h3 className="service-title">Cyber Security</h3>
                 </div>
                 <div className="service-item">
                     <span className="service-icon">☁️</span>
@@ -19,7 +19,13 @@ const AboutMe = () => {
             <div className="about-me-right">
                 <h2 className="about-title">About me</h2>
                 <p className="about-paragraph">
-                    I am a Computer Science student from India, currently focusing on Cyber Security, IoT, and Machine Learning. I am eager to collaborate on projects related to Machine Learning, IoT, and Cyber Security. I am also seeking assistance with Defensive Hacking and Machine Learning. My current learning journey includes Rust, Machine Learning, Artificial Intelligence, and Defensive Hacking. Feel free to connect with me to discuss Cars, Books, and Technology.
+                    I am a Computer Science student from India, 
+                    currently focusing on Cyber Security, IoT, and 
+                    Machine Learning. I am eager to collaborate on projects r
+                    elated to Machine Learning, IoT, and Cyber Security. I am also 
+                    seeking assistance with Defensive Hacking and Machine Learning. 
+                    My current learning journey includes Rust, Machine Learning, Artificial Intelligence, 
+                    and Defensive Hacking. Feel free to connect with me to discuss Cars, Books, and Technology.
 
 
                     
@@ -31,7 +37,7 @@ const AboutMe = () => {
                  </div>
                  <div className="stat-item">
                     <strong className="stat-value">1</strong>
-                    <p className="stat-value">Year of Exprience</p>
+                    <p className="stat-value">Year of Experience</p>
                  </div>
                 </div>
             </div>
